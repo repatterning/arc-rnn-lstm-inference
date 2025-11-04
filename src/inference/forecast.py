199716935236d307ce3925123dbe30fa14c93c04
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-11-04 23:41:29 +0000

    Update src/inference/forecast.py: In Progress

--- src/inference/forecast.py
+++ src/inference/forecast.py
@@ -1,12 +1,54 @@
 
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+
 import src.elements.master as mr
+import src.elements.attribute as atr
 
 
 class Forecast:
 
-    def __init__(self):
-        pass
+    def __init__(self, arguments: dict, attribute: atr.Attribute):
+        """
 
-    def exc(self, master: mr.Master):
+        :param arguments:
+        :param attribute:
+        """
+
+
+        self.__attribute = attribute
+        self.__arguments = arguments
+
+        # ...
+        self.__n_points_future = self.__arguments.get('n_points_future')
+        self.__cutoff = self.__arguments.get('n_points_future') + self.__attribute.modelling.get('n_sequence')
+
+    def __get_structure(self, frame: pd.DataFrame) -> pd.DataFrame:
+        """
+
+        :param frame:
+        :return:
+        """
+
+        dates = pd.date_range(start=frame['date'].max(), periods=self.__n_points_future+1, freq='h', inclusive='right')
+        timestamps = (dates.astype(np.int64) / (10 ** 6)).astype(np.longlong)
+        tail = pd.DataFrame(data={'timestamp': timestamps, 'date': dates})
+
+        for i in self.__attribute.modelling.get('targets'):
+            tail.loc[:, i] = np.nan
+
+        __structure = pd.concat([frame.copy(), tail], axis=0, ignore_index=True)
+
+        return __structure.copy()[-self.__cutoff:]
+
+    def exc(self, model: tf.keras.models.Sequential, master: mr.Master):
+        """
+        
+        :param model:
+        :param master:
+        :return:
+        """
 
         frame = master.transforms
+        structure = self.__get_structure(frame=frame)
