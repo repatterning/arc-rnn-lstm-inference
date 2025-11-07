f576bbb7eb948177b0247491d5dc51acd9114b2a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-11-07 18:02:24 +0000

    Update src/inference/interface.py

--- src/inference/interface.py
+++ src/inference/interface.py
@@ -1,20 +1,18 @@
 """Module inference/interface.py"""
 import logging
 
-import os
 import dask
 import pandas as pd
 
-import config
+import src.elements.approximations as apr
 import src.elements.attribute as atr
 import src.elements.master as mr
 import src.elements.specification as sc
-import src.elements.approximations as apr
 import src.inference.approximating
 import src.inference.attributes
 import src.inference.data
-import src.inference.scaling
 import src.inference.persist
+import src.inference.scaling
 
 
 class Interface:
@@ -33,25 +31,7 @@ class Interface:
         self.__limits = limits
 
         # Setting up
-        self.__configurations = config.Config()
-        self.__endpoint: str = self.__arguments.get('additions').get('modelling_data_source')
         self.__scaling = src.inference.scaling.Scaling()
-
-    @dask.delayed
-    def __get_listing(self, specification: sc.Specification) -> list[str]:
-        """
-
-        :param specification:
-        :return:
-        """
-
-        listing  = [os.path.join(self.__configurations.data_, 'source', str(specification.catchment_id), str(specification.ts_id), f'{limit}.csv' )
-         for limit in self.__limits]
-
-        # listing = [f'{self.__endpoint}/{specification.catchment_id}/{specification.ts_id}/{limit}.csv'
-        #            for limit in self.__limits ]
-
-        return listing
 
     @dask.delayed
     def __set_transforms(self, data: pd.DataFrame, scaling: dict) -> mr.Master:
