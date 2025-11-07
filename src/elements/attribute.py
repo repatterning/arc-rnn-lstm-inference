e2fc472bb2e7e6c75cbc2af0cf94a973bae9f095
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-11-07 17:51:15 +0000

    Update src/elements/attribute.py

--- src/elements/attribute.py
+++ src/elements/attribute.py
@@ -1,7 +1,20 @@
-
+"""Module attribute.py"""
 import typing
 
 class Attribute(typing.NamedTuple):
+    """
+    The data type class ⇾ Attribute<br><br>
+
+    Attributes<br>
+    ----------<br>
+
+    modelling : dict
+        The dictionary of a model's settings.
+    scaling : dict
+        The dictionary of the scaling arguments due to, derived from, a model's training data
+    n_points_future : int
+        The number of future points to predict
+    """
 
     modelling: dict
     scaling: dict
